813d325b0767739bbd5f3e174c6fe79b44eec150
Ricardo Sarto <rsarto-ga@guardiananalytics.com>
2020-01-17 08:27:38 -0300

    commands for replica set

---
+++ replica.js
@@ -0,0 +1,17 @@
+/*
+ * Access any instance from mongo cluster and from the shell execute the commands below one by one.
+ */
+//Creates the replica set.
+rs.initiate();
+
+//Adds the host mongodb_rpl_1 to the replica set.
+rs.add('mongodb_rpl_1');
+
+//Adds the host mongodb_rpl_2 to the replica set.
+rs.add('mongodb_rpl_2');
+
+//Shows information about synced replicas.
+rs.printSlaveReplicationInfo();
+
+//Shows information about the replica set.
+db.runCommand("ismaster");
